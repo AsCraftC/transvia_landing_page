bcee9fe350d740c81afbda55627a4196043fc6c8
Cristobal Contreras <ascraft.contreras@outlook.cl>
2024-10-24 21:04:26 -0300

    feat: Maquetación de Header y Footer version mobile
    - También incluye los tag desktop pero se dejaron escondidos
    - Se movieron los assets a carpetas por pagina para búsqueda más fácil

---
+++ src/data/home.ts
@@ -0,0 +1,13 @@
+import type { CompanyData , HomeData } from "./interfaces";
+
+export const companyData : CompanyData = {
+    companyName : "Transvia Ingeniería",
+    email       : "direccion@dominio.cl",
+    phone       : "+56 9 1234 5678",
+    location    : "Dirección 123",
+    city        : "Temuco, Chile"
+}
+
+export const homeData : HomeData = {
+    eslogan: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+}
---
+++ src/data/interfaces.ts
@@ -0,0 +1,12 @@
+export interface HomeData {
+    eslogan: string;
+    
+}
+
+export interface CompanyData{
+    companyName : string;
+    email       : string;
+    phone       : string;
+    location    : string;
+    city        : string;
+}
